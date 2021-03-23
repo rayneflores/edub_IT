d55758e83d1f8e3464b5a41d393b86f3876ebf19
Diego <dsalbarrey@gmail.com>
2021-03-22 23:18:25 -0300

    Primer commit

---
+++ app/src/main/java/com/edubit/curriculapp/Aprobe.java
@@ -0,0 +1,14 @@
+package com.edubit.curriculapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aprobe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprobe);
+    }
+}
---
+++ app/src/main/java/com/edubit/curriculapp/QuieroCursar.java
@@ -0,0 +1,14 @@
+package com.edubit.curriculapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuieroCursar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiero_cursar);
+    }
+}
