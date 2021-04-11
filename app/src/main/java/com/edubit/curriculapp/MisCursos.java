fb560b9b7b450ac632a7f0ab14b9c6630ce44f41
Lucia <lucia.salbarrey@gmail.com>
2021-04-11 00:01:03 -0300

    edite mis cursos

--- app/src/main/java/com/edubit/curriculapp/MisCursos.java
+++ app/src/main/java/com/edubit/curriculapp/MisCursos.java
@@ -11,4 +11,20 @@ public class MisCursos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_cursos);
     }
+
+    /*
+    txt = (TextView)findViewById(R.id.txt);
+SpannableString content = new SpannableString(txt.getText());
+content.setSpan(new UnderlineSpan(), 0, txt.length(), 0);
+txt.setText(content);
+txt.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+Intent i = new Intent(Intent.ACTION_VIEW);
+i.setData(Uri.parse("http://www.bedelias.edu.uy"));
+startActivity(i);
+}
+});
+    */
+
 }
