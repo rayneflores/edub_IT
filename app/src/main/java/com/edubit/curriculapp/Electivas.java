38d71178cc66b470916aa234ebb4b338f72f0406
Lucia <lucia.salbarrey@gmail.com>
2021-04-23 18:57:36 -0300

    agrego config electivas e inform

--- app/src/main/java/com/edubit/curriculapp/Electivas.java
+++ app/src/main/java/com/edubit/curriculapp/Electivas.java
@@ -3,6 +3,8 @@ package com.edubit.curriculapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Electivas extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Electivas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electivas);
+    }
+
+    public void saveTostElectivas(View view) {
+        Toast.makeText(getApplicationContext(), R.string.save, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/edubit/curriculapp/Optativas.java
+++ app/src/main/java/com/edubit/curriculapp/Optativas.java
@@ -3,6 +3,8 @@ package com.edubit.curriculapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Optativas extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Optativas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_optativas);
+
+
+    }
+    public void saveTostOptativas(View view) {
+        Toast.makeText(getApplicationContext(), R.string.save, Toast.LENGTH_LONG).show();
+
     }
 }
