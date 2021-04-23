b04c253f9b6acb42609a6359689c8fbb5de8975b
Lucia <lucia.salbarrey@gmail.com>
2021-04-23 13:15:43 -0300

    tost simulacion

--- app/src/main/java/com/edubit/curriculapp/MapaCurricular.java
+++ app/src/main/java/com/edubit/curriculapp/MapaCurricular.java
@@ -3,6 +3,8 @@ package com.edubit.curriculapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MapaCurricular extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class MapaCurricular extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa_curricular);
 
+    }
+
+    public void saveTost(View view) {
+        Toast.makeText(getApplicationContext(), R.string.save, Toast.LENGTH_LONG).show();
     }
 }
